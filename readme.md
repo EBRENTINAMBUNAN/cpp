3c9c7f7ec466e9feec212ab761f2ee8b77e0cec3
EBRENTINAMBUNAN <ebrentinambunan2003@gmail.com>
2024-09-21 17:48:08 +0700

    projek cpp

---
+++ readme.md
@@ -0,0 +1,14 @@
+## Informasi NameCode
+
+### struktur.cpp
+Adalah sebuah program untuk melihat detail kota sesuai kota yang diinputkan.
+
+---
+
+### p2.cpp
+Adalah sebuah program untuk menerima input 5 angka dari pengguna, menampilkan angka-angka yang dimasukkan, menghitung dan menampilkan jumlah totalnya, serta menemukan dan menampilkan nilai maksimum dari angka-angka tersebut.
+
+---
+
+### nestruk.cpp
+Adalah sebuah program untuk mengelola data mahasiswa, termasuk nama, NIM, nilai tugas, dan nilai ujian. Program ini juga menghitung nilai akhir berdasarkan bobot tugas (40%) dan ujian (60%) serta menampilkan data lengkap mahasiswa beserta nilai akhir mereka.
--- struktur.cpp
+++ struktur.cpp
@@ -1,36 +1,74 @@
 #include <iostream>
+#include <map>
 using namespace std;
 
-struct provinsi
+struct Provinsi
 {
-    string kota;
     string namaProvinsi;
     double luasWilayah;
     long jumlahPenduduk;
+    string namaWalikota;
+    string kodePos;
+    int tahunBerdiri;
 };
 
 int main()
 {
-    provinsi dataProvinsi;
+    map<string, Provinsi> dataProvinsi = {
+        {"tangerang selatan", {"Banten", 147.19, 1500000, "Benyamin Davnie", "15310", 2008}},
+        {"medan", {"Sumatera Utara", 265.10, 2500000, "Bobby Nasution", "20111", 1590}},
+        {"surabaya", {"Jawa Timur", 350.54, 2890000, "Eri Cahyadi", "60119", 1293}},
+        {"bandung", {"Jawa Barat", 167.67, 2390000, "Yana Mulyana", "40111", 1810}},
+        {"jakarta", {"DKI Jakarta", 662.33, 10770487, "Heru Budi Hartono", "10110", 1527}},
+        {"semarang", {"Jawa Tengah", 373.78, 1670000, "Hendrar Prihadi", "50135", 1547}},
+        {"makassar", {"Sulawesi Selatan", 199.26, 1410000, "Moh. Ramdhan Pomanto", "90111", 1607}},
+        {"denpasar", {"Bali", 123.98, 897300, "I Gusti Ngurah Jaya Negara", "80232", 1788}},
+        {"palembang", {"Sumatera Selatan", 358.55, 1640000, "Harnojoyo", "30111", 683}},
+        {"yogyakarta", {"DI Yogyakarta", 32.8, 373589, "Haryadi Suyuti", "55111", 1756}},
+        {"balikpapan", {"Kalimantan Timur", 503.3, 645800, "Rahmad Mas'ud", "76112", 1897}},
+        {"malang", {"Jawa Timur", 252.1, 869000, "Sutiaji", "65111", 1914}},
+        {"batam", {"Kepulauan Riau", 715, 1326000, "Muhammad Rudi", "29461", 1970}},
+        {"pekanbaru", {"Riau", 632.26, 964000, "Firdaus", "28111", 1784}},
+        {"manado", {"Sulawesi Utara", 157.26, 451916, "Andrei Angouw", "95111", 1623}},
+        {"pontianak", {"Kalimantan Barat", 107.82, 655000, "Edi Rusdi Kamtono", "78111", 1771}},
+        {"padang", {"Sumatera Barat", 694.96, 909000, "Hendri Septa", "25111", 1669}},
+        {"samarinda", {"Kalimantan Timur", 718, 812597, "Andi Harun", "75111", 1668}},
+        {"banda aceh", {"Aceh", 61.36, 224000, "Aminullah Usman", "23111", 1205}},
+        {"jayapura", {"Papua", 935.92, 315872, "Benhur Tommy Mano", "99112", 1910}}};
 
-    cout << "Masukkan Nama Kota: ";
-    getline(cin, dataProvinsi.kota);
+    char pilihan;
+    do
+    {
+        string kota;
+        cout << "Masukkan Nama Kota: ";
+        getline(cin, kota);
 
-    if (dataProvinsi.kota == "tangerang selatan")
-    {
-        dataProvinsi.namaProvinsi = "Banten";
-        dataProvinsi.luasWilayah = 147.19;
-        dataProvinsi.jumlahPenduduk = 1500000;
-    }
-    else
-    {
-        cout << "Maaf, Kota tidak tersedia." << endl;
-        return 0;
-    }
-    cout << "\nData Provinsi:" << endl;
-    cout << "Kota: " << dataProvinsi.kota << endl;
-    cout << "Nama Provinsi: " << dataProvinsi.namaProvinsi << endl;
-    cout << "Luas Wilayah: " << dataProvinsi.luasWilayah << " km²" << endl;
-    cout << "Jumlah Penduduk: " << dataProvinsi.jumlahPenduduk << endl;
+        auto it = dataProvinsi.find(kota);
+        if (it != dataProvinsi.end())
+        {
+            cout << "\nData-data Kota:" << endl;
+            cout << "=========================" << endl;
+            cout << "Nama Kota: " << kota << endl;
+            cout << "Nama Provinsi: " << it->second.namaProvinsi << endl;
+            cout << "Luas Wilayah: " << it->second.luasWilayah << " km²" << endl;
+            cout << "Jumlah Penduduk: " << it->second.jumlahPenduduk << endl;
+            cout << "Nama Walikota: " << it->second.namaWalikota << endl;
+            cout << "Kode Pos: " << it->second.kodePos << endl;
+            cout << "Tahun Berdiri: " << it->second.tahunBerdiri << endl;
+        }
+        else
+        {
+            cout << "Maaf, Kota tidak tersedia. Silakan coba lagi.\n"
+                 << endl;
+        }
+
+        cout << "\nApakah Anda ingin memasukkan data lagi? (y/n): ";
+        cin >> pilihan;
+        cin.ignore();
+
+    } while (pilihan == 'y' || pilihan == 'Y');
+
+    cout << "Terima kasih! Program berakhir." << endl;
+
     return 0;
 }
