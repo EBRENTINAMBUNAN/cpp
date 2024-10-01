7596e4ad7a5e241a372c59e2d17416ace6403a90
EBRENTINAMBUNAN <ebrentinambunan2003@gmail.com>
2024-10-01 07:14:02 +0700

    new

---
+++ p5.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    const int angka = 5;
+    int array[angka];
+    int searchValue;
+    bool found = false;
+
+    cout << "========================================" << endl;
+    cout << "  Selamat Datang di Program Pencari Nilai" << endl;
+    cout << "    Maksimum dan Minimum dari 5 Angka!" << endl;
+    cout << "          by Ebren Tinambunan          " << endl;
+    cout << "========================================" << endl;
+
+    cout << "\nMasukkan 5 angka:" << endl;
+    for (int i = 0; i < angka; i++)
+    {
+        cout << "Masukkan nilai pada indeks " << i << ": ";
+        cin >> array[i];
+    }
+
+    cout << "\nMasukkan nilai yang akan dicari: ";
+    cin >> searchValue;
+
+    for (int i = 0; i < angka; i++)
+    {
+        if (array[i] == searchValue)
+        {
+            cout << "Nilai ditemukan pada indeks ke-" << i << " dalam array." << endl;
+            found = true;
+            break;
+        }
+    }
+
+    if (!found)
+    {
+        cout << "Nilai tidak ditemukan dalam array." << endl;
+    }
+
+    return 0;
+}
